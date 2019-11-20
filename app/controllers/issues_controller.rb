8ef0aeb7661edfa48ad9750ee80aa42512c39955
Sara <theredwolfblake66@hotmail.com>
2019-11-20 17:17:12 +0100

    assigne

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,5 @@
 class IssuesController < ApplicationController
   before_action :set_issue, only: [:show, :edit, :update, :destroy]
-
   # GET /issues
   # GET /issues.json
   def index
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ApplicationRecord
-	belongs_to :user
+	#belongs_to :user
     belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id, optional: true
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
