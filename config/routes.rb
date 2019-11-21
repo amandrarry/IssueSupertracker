e0d2d1eb3f61a1c8dae6875fea200288322feb36
Sara <theredwolfblake66@hotmail.com>
2019-11-21 12:53:14 +0100

    Static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   resources :users
   resources :issues
   resources :issues do
     resources :comments
   end
   get '/users/current_user' => "users#the_current_user", as: :current_user
+  get  'static_pages/home'
+  get  'static_pages/help'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'issues#index'
+  root 'static_pages#home'
   
   # Routes for Google authentication
   #get ‘auth/:provider/callback’, to: ‘sessions#googleAuth’
@@ -16,6 +21,10 @@ Rails.application.routes.draw do
   post '/issues/:id/vote' => "issues#vote", as: :vot
   post '/issues/:id/watcher/:index' => "issues#watcher", as: :watch
   put '/issues/:id/status' => "issues#update_status", as: :update_status
+
+  put '/issues' => 'application#set_current_user', as: :log
+
+
 
  
 
